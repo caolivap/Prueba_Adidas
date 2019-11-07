2bfcfb48984116e7d1036f2ce163d036c9eb41c1
Carlos Oliva <caolivap@unal.edu.co>
2019-11-07 18:49:41 -0500

    Se ultiman las configuraciones del serenity.properties, se modifica el Gherkin del escenario el nombramiento de los métodos en el stepDefinitions y se agregan Targets en la clase AviancaHomePage

--- src/test/java/com/devco/reservavuelo/stepdefinitions/ReservaVueloStepDefinitions.java
+++ src/test/java/com/devco/reservavuelo/stepdefinitions/ReservaVueloStepDefinitions.java
@@ -1,13 +1,20 @@
 package com.devco.reservavuelo.stepdefinitions;
 
 import com.devco.reservavuelo.tasks.Abrir;
+import com.devco.reservavuelo.tasks.Consultar;
+import com.devco.reservavuelo.tasks.Elegir;
+import com.devco.reservavuelo.tasks.IngresarDatos;
+import com.devco.reservavuelo.ui.ResultadoViajesPage;
+import com.devco.reservavuelo.utils.UtilidadesFechas;
 import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
+import java.text.ParseException;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.thucydides.core.annotations.Managed;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.WebDriver;
 
 public class ReservaVueloStepDefinitions {
@@ -15,31 +22,35 @@ public class ReservaVueloStepDefinitions {
   @Managed(driver = "chrome")
   private WebDriver suNavegador;
   private Actor carlos = Actor.named("Carlos");
+  private ResultadoViajesPage resultadoViajesPage = new ResultadoViajesPage();
 
   @Before
-  public void configuracionInicial(){
+  public void configuracionInicial() {
     carlos.can(BrowseTheWeb.with(suNavegador));
   }
 
   @Dado("^que (.*) se encuentra en la página inicial de la aerolínea Avianca$")
-  public void queCarlosSeEncuentraEnLaPáginaInicialDeLaAerolíneaAvianca(String nombreActor){
+  public void queCarlosSeEncuentraEnLaPáginaInicialDeLaAerolíneaAvianca(String nombreActor) {
     carlos.wasAbleTo(Abrir.LaPaginaDeAvianca());
   }
 
-
-  @Cuando("^busca vuelos de ida desde (.*) hacia (.*)$")
-  public void buscaVuelosDeIdaDesdeMedellínHaciaBogotá(String ciudadOrigen, String ciudadDestino) {
+  @Cuando("^busca vuelos (.*) desde (.*) hacia (.*) para dentro de (.*) días$")
+  public void buscaVuelosDeIdaDesdeOrigenHaciaDestino(String tipoViaje, String ciudadOrigen, String ciudadDestino,
+      int dias) throws ParseException {
+    String fechaVuelo = UtilidadesFechas.obtenerFechaDeVuelo(dias);
+    carlos.attemptsTo(
+        Elegir.SoloViajeDeIda(),
+        IngresarDatos.deVuelo(ciudadOrigen, ciudadDestino, fechaVuelo),
+        Consultar.Vuelos());
+    //TODO convertir el número de días a una fecha con el formato adecuado
 
   }
 
-  @Cuando("^reserva el vuelo más económico$")
-  public void reservaElVueloMásEconómico(){
-
-  }
-
-  @Entonces("^se genera un número de reserva exitosamente$")
-  public void seGeneraUnNúmeroDeReservaExitosamente(){
-
+  @Entonces("^se obtienen los vuelos disponibles para esa fecha$")
+  public void seObtienenLosVuelosDisponiblesParaEsaFecha() {
+    //carlos.should(seeThat(HayDisponibilidad.deVuelos());
+    MatcherAssert.assertThat("No se encontraron viajes para la fecha", !resultadoViajesPage.getLstViajes().isEmpty());
+    //System.out.println(HayDisponibilidad.deVuelos());
   }
 
 
--- src/test/resources/features/realizar_reserva_vuelo_avianca.feature
+++ src/test/resources/features/realizar_reserva_vuelo_avianca.feature
@@ -1,17 +1,16 @@
 #language: es
 
-Característica: Realizar una reserva de vuelo en Avianca
+Característica: Realizar búsqueda de vuelos en Avianca
   Yo como usuario de Avianca
-  Quiero buscar el vuelo más económico entre dos ciudades dadas
-  Para reservar con anticipación mi vuelo
+  Quiero buscar vuelos entre dos ciudades dadas
+  Para conocer la disponibilidad de vuelos en una fecha establecida
 
   @Escenario1
   Esquema del escenario: Reserva exitosa de vuelo
     Dado que Carlos se encuentra en la página inicial de la aerolínea Avianca
-    Cuando busca vuelos de ida desde <CiudadOrigen> hacia <CiudadDestino>
-    Y reserva el vuelo más económico
-    Entonces se genera un número de reserva exitosamente
+    Cuando busca vuelos solo de ida desde <Ciudad Origen> hacia <Ciudad Destino> para dentro de <Número Días> días
+    Entonces se obtienen los vuelos disponibles para esa fecha
 
     Ejemplos:
-      | CiudadOrigen | CiudadDestino |
-      | Medellín     | Bogotá        |
+      | Ciudad Origen | Ciudad Destino | Número Días |
+      | Medellín      | Bogotá         | 40          |
